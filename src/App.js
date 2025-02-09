7a5a40f5ba5cbdfa946a967de015853be6845514
Bigyetiwash <Bigyetiwash@outlook.com>
2025-02-09 15:02:01 +1030

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardContent } from "./components/ui/card";
+import { Card, CardContent } from "./components/ui/Card";
 import { Button } from "./components/ui/button";
 import { InstagramEmbed } from "react-social-media-embed";
 import { FacebookEmbed } from "react-social-media-embed";
