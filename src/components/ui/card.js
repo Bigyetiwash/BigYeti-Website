284701f84f008d7ef4747db3f3b0cc8e95af8e70
Bigyetiwash <Bigyetiwash@outlook.com>
2025-02-09 14:18:30 +1030

    Delete src/components directory

--- src/components/ui/card.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Card({ children }) {
-  return <div className="bg-gray-100 p-4 rounded shadow">{children}</div>;
-}
-
-export function CardContent({ children }) {
-  return <div>{children}</div>;
-}
